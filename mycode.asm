c7bb3d8503206b5dfa6c8b7e844203dd7701db67
divyavenkatraman <44758649+divyavenkatraman@users.noreply.github.com>
2021-12-05 01:03:59 -0500

    Update mycode.asm

--- mycode.asm
+++ mycode.asm
@@ -3,8 +3,6 @@ addi $2, 0x5
 sw $1, 0($2) 
 clr $1
 clr $2 
-
-
 addi $1, 0x4
 addi $2, 0x5
 lw $1, 0($1)
@@ -13,23 +11,17 @@ add $3, $1, $2
 clr $1 
 addi $1, 0x11 
 sw $3, 0($1) 
-
-
 inv $2, $2 
 addi $2, 0x1 
 clr $1
 addi $1, 0x12
 sw $2, 0($1)
-
-
 lw $2, 0($1) 
 inv $2, $2 
 addi $2, 0x1 
-
 clr $1 
 addi $1, 0x4 
 lw $1, 0($1) 
-
 add $3, $2, $1
 clr $1
 addi $1, 0x13
