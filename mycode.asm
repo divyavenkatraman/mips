42224b3aa366ca5f0092dfefbaeca78610513fdb
divyavenkatraman <44758649+divyavenkatraman@users.noreply.github.com>
2021-12-09 02:18:19 -0500

    Update mycode.asm

--- mycode.asm
+++ mycode.asm
@@ -1,28 +1,28 @@
-addi $1, 0x0F 
-addi $2, 0x5 
-sw $1, 0($2) 
-clr $1
-clr $2 
-addi $1, 0x4
-addi $2, 0x5
-lw $1, 0($1)
-lw $2, 0($2)
-add $3, $1, $2 
-clr $1 
-addi $1, 0x11 
-sw $3, 0($1) 
-inv $2, $2 
-addi $2, 0x1 
-clr $1
-addi $1, 0x12
-sw $2, 0($1)
-lw $2, 0($1) 
-inv $2, $2 
-addi $2, 0x1 
-clr $1 
-addi $1, 0x4 
-lw $1, 0($1) 
-add $3, $2, $1
-clr $1
-addi $1, 0x13
-sw $3, 0($1)
+
+addi $1, 0x5
+addi $2, 0x4
+sw $1, 0($2
+clr $2
+sra $2, $1, 0x8
+addi $3, 0xFF
+beq $2, $3,firstnumneg
+clr $3
+beq $2, $3,firstnumpos
+firstnumpos:
+addi $2, 0x0F
+addi $0, 0x0
+addi $3, 0x0
+posloop:
+beq $0, $1, end
+add $3, $3, $2 
+addi $0, 0x1
+beq $1, $1, posloop
+firstnumneg:
+addi $2, 0x0F
+addi $0, 0x0
+addi $3, 0x0
+negloop:
+beq $0, $1, end
+add $3, $3, $2 
+addi $0, 0xFF
+beq $1, $1, negloop
